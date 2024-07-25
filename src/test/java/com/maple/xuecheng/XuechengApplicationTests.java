de64a5f971884407379856c19bfe12a1c2988beb
侯德伟 <20225278@s.hlju.edu.cn>
2024-07-25 16:48:55 +0800

    the one day

---
+++ src/test/java/com/maple/xuecheng/XuechengApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maple.xuecheng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XuechengApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
